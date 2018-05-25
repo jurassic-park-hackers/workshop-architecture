b4bfadbae10f6b32abc991c69210901309309a50
Gabriel Batista de Lima <dev.gabriel.lima@gmail.com>
2018-05-25 17:31:38 -0300

    models and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+Customer.destroy_all
+OrderProduct.destroy_all
+Order.destroy_all
+
+Product.create!([
+  { name: 'iPhone 8', price: 700.0 },
+  { name: 'iPhone X', price: 1099.0 },
+  { name: 'Galaxy S9', price: 799.0 },
+  { name: 'Galaxy S9 Plus', price: 899.0 },
+])
+
+Customer.create!([
+  { name: 'John' },
+  { name: 'Mary' },
+])
