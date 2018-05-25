a7bb62adc9d70033cd988f6c52a6bab6a52eb631
Gabriel Batista de Lima <dev.gabriel.lima@gmail.com>
2018-05-25 18:39:50 -0300

    present error when product not found

--- spec/lib/orders/create_order_usecase_spec.rb
+++ spec/lib/orders/create_order_usecase_spec.rb
@@ -5,15 +5,26 @@ class CreateOrderUsecase
   end
 
   def execute(customer_id, products)
-    @presenter.show_error_customer_not_found
+    if not @customer_gateway.customer_exists?(customer_id)
+      @presenter.show_error_customer_not_found
+    end
+
+    for product in products
+      @presenter.show_error_product_not_found(product[:product_id])
+    end
   end
 end
 
 class CustomerGateway
+  def customer_exists?(customer_id)
+  end
 end
 
 class CreateOrderPresenter
   def show_error_customer_not_found
+  end
+
+  def show_error_product_not_found(product_id)
   end
 end
 
@@ -26,8 +37,19 @@ RSpec.describe CreateOrderUsecase do
     it 'customer not found' do
       customer_id = 999
       products = []
+      allow(customer_gateway).to receive(:customer_exists?).with(customer_id).and_return(false)
 
       expect(presenter).to receive(:show_error_customer_not_found)
+
+      usecase.execute(customer_id, products)
+    end
+
+    it 'product not found' do
+      customer_id = 1
+      products = [{ product_id: 555, quantity: 1 }]
+      allow(customer_gateway).to receive(:customer_exists?).with(customer_id).and_return(true)
+
+      expect(presenter).to receive(:show_error_product_not_found).with(555)
 
       usecase.execute(customer_id, products)
     end
