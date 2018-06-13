a2ba2188ef87285882d6158ff6c1c7a9c6b7cd95
hudolfhess <hudolf@gmail.com>
2018-06-13 14:26:53 -0300

    Created CreateOrderPresenterJson

---
+++ spec/presenters/orders/create_order_presenter_spec.rb
@@ -0,0 +1,134 @@
+require './lib/orders/presenters'
+
+class CreateOrderPresenterJson < CreateOrderPresenter
+  def initialize
+    @status = :bad_request
+    @errors = []
+    @order = {:order_id => nil, :total_price => nil}
+  end
+
+  def show_error_customer_not_found
+    @status = :not_found
+    @errors += ['Customer not found.']
+  end
+
+  def show_error_product_not_found(product_id)
+    @status = :not_found
+    @errors += ['Product not found: ' + product_id.to_s]
+  end
+
+  def show_order(order_id, total_price)
+    @status = :ok
+    @order[:order_id] = order_id
+    @order[:total_price] = total_price
+  end
+
+  def response
+    {
+      'status': @status,
+      'data': create_data_response
+    }
+  end
+
+  private
+  
+  def create_data_response
+    if @errors.empty?
+      if @order[:order_id].nil?
+        data = {}
+      else
+        data = @order
+      end
+    else
+      data = {:errors => @errors}
+    end
+
+    data
+  end
+end
+
+RSpec.describe CreateOrderPresenterJson do
+  let(:presenter) { described_class.new() }
+
+  describe '#respond' do
+    context 'when nothing is presented' do
+      it 'http status should be bad request' do
+        response = presenter.response
+
+        expect(response[:status]).to eq(:bad_request)
+      end
+
+      it 'response should be empty' do
+        response = presenter.response
+
+        expect(response[:data]).to eq({})
+      end
+    end
+
+    context 'when customer not found is presented' do
+      before(:each) do
+        presenter.show_error_customer_not_found
+      end
+
+      it 'http status should be not found' do
+        response = presenter.response
+
+        expect(response[:status]).to eq(:not_found)
+      end
+
+      it 'response with customer not found error' do
+        response = presenter.response
+
+        expect(response[:data]).to eq({:errors => ['Customer not found.']})
+      end
+    end
+
+    context 'when product not found is presented' do
+      it 'http status should be not found' do
+        presenter.show_error_product_not_found(123)
+
+        response = presenter.response
+
+        expect(response[:status]).to eq(:not_found)
+      end
+
+      it 'response product with error' do
+        presenter.show_error_product_not_found(123)
+
+        response = presenter.response
+
+        expect(response[:data]).to eq({:errors => ['Product not found: 123']})
+      end
+
+      it 'response products with error when two products presented with error' do
+        presenter.show_error_product_not_found(123)
+        presenter.show_error_product_not_found(478)
+
+        response = presenter.response
+
+        expect(response[:data]).to eq({:errors => ['Product not found: 123', 'Product not found: 478']})
+      end
+    end
+
+    context 'when order is presented' do
+      let(:order_id) { 890 }
+      let(:total_price) { 78912.78}
+
+      before(:each) do
+        presenter.show_order(order_id, total_price)
+      end
+
+      it 'http status should be ok' do
+        response = presenter.response
+
+        expect(response[:status]).to eq(:ok)
+      end
+
+      it 'response with order id and total_price' do
+        response = presenter.response
+
+        expect(response[:data]).to eq({:order_id => order_id, :total_price => total_price})
+      end
+    end
+  end
+end
