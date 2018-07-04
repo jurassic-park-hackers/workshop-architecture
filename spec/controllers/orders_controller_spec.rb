f73c2334125fa72163c80ec397321310190115f3
Gabriel Batista de Lima <dev.gabriel.lima@gmail.com>
2018-07-04 14:46:15 -0300

    fix spacing and variable in camel case

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe OrdersController do
-    describe 'create order' do
-        it 'create order with products and customer' do
-            product = Product.create(price: 100.0)
-            customer = Customer.create()
-            products_id_with_quantity = [{product_id: product.id, quantity: 20}]
+  describe 'create order' do
+    it 'create order with products and customer' do
+      product = Product.create(price: 100.0)
+      customer = Customer.create()
+      products_id_with_quantity = [{product_id: product.id, quantity: 20}]
 
-            post :create, as: :json, customer_id: customer.id, products_id_with_quantity: products_id_with_quantity, format: :json
+      post :create, as: :json, customer_id: customer.id, products_id_with_quantity: products_id_with_quantity, format: :json
 
-            responseBody = ActiveSupport::JSON.decode(response.body)
-            expect(response.status).to eq(200)
-            expect(responseBody["order_id"]).to_not eq(nil)
-            expect(responseBody["total_price"]).to eq("2000.0")
-        end
+      response_body = ActiveSupport::JSON.decode(response.body)
+      expect(response.status).to eq(200)
+      expect(response_body["order_id"]).to_not eq(nil)
+      expect(response_body["total_price"]).to eq("2000.0")
     end
+  end
 end
