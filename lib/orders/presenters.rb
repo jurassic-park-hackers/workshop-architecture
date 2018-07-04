aad3a72e5f267b51fb193e82f4a6f4364c541982
Gabriel Batista de Lima <dev.gabriel.lima@gmail.com>
2018-07-04 16:20:55 -0300

    extrated use case

--- lib/orders/presenters.rb
+++ lib/orders/presenters.rb
@@ -3,3 +3,8 @@ class CreateOrderPresenter
   def show_error_product_not_found(product_id); end
   def show_order(order_id, total_price); end
 end
+
+class ShowOrdersByCustomerPresenter
+  def show_orders(orders); end
+  def error_customer_not_found(customer_id); end
+end
