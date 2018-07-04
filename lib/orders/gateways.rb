aa24de1e234e09a739f8a028a3d3ad175161612d
Gabriel Batista de Lima <dev.gabriel.lima@gmail.com>
2018-07-04 16:12:50 -0300

    show orders by customer use case

--- lib/orders/gateways.rb
+++ lib/orders/gateways.rb
@@ -1,3 +1,4 @@
 class OrderGateway
   def save_order(customer_id, order_products); end
+  def get_orders_by_customer_id(customer_id); end
 end
--- lib/orders/structs.rb
+++ lib/orders/structs.rb
@@ -1,3 +1,8 @@
 ProductStruct = Struct.new(:product_id, :name, :price)
 
 OrderProductStruct = Struct.new(:product_id, :quantity, :price)
+
+OrderGatewayStruct = Struct.new(:order_id, :customer_id, :order_products)
+
+ShowOrdersByCustomerResponse = Struct.new(:orders_response)
+OrderResponse = Struct.new(:order_id, :customer_id, :total_price, :order_products)
